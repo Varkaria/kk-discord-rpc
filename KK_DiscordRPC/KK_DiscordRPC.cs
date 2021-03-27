5596a9137b7bdfea92144a3c9acbda2ed36f90b4
simp sylphiette <61514399+Varkaria@users.noreply.github.com>
2021-03-27 21:37:18 +0700

    Add Files! [Not working right now]

---
+++ KK_DiscordRPC/KK_DiscordRPC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using DiscordRPC;
+
+namespace KK_DiscordRPC
+{
+    [BepInPlugin(GUID, "Koikatsu: Discord Rich Presence", Version)]
+    [BepInDependency(KKAPI.KoikatuAPI.GUID, KKAPI.KoikatuAPI.VersionConst)]
+    public partial class Koi_DiscordRPC : BaseUnityPlugin
+    {
+        public const string GUID = "varkaria.DiscordRPC";
+        public const string Version = "1.0";
+
+        internal static Koi_DiscordRPC Instance;
+        internal static new ManualLogSource Logger;
+        
+        public static readonly DiscordRpc.RichPresence Presence = new DiscordRpc.RichPresence();
+
+        private void Awake()
+        {
+            Instance = this;
+            Logger = base.Logger;
+            
+            var handlers = new DiscordRpc.EventHandlers();
+            DiscordRpc.Initialize(
+                "604588957208150016",
+                ref handlers,
+                false,
+                "643270");
+
+            Console.WriteLine("Current gamemode is {0}", KKAPI.KoikatuAPI.GetCurrentGameMode());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Koikatsu: Discord Rich Presence
+i will make readme.md when it's working
+now it's not working : (
+
